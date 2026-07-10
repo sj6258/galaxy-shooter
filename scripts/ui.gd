46dfb30140095f1b6691fad185e3ac235cfa7300
Sj6258 <samriddhjoneja@gmail.com>
2026-07-11 02:44:41 +0530

    stated making lasers and finished main screen

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -25,3 +25,34 @@ func _on_button_ship_three_pressed() -> void:
 	$CanvasLayer/choosescreen/ships/shipOne.visible = false
 	$CanvasLayer/choosescreen/ships/shipTwo.visible = false
 	$CanvasLayer/choosescreen/ships/shipThree.visible = true
+
+
+func _on_button_choose_pressed() -> void:
+	global.game_on = true
+	$CanvasLayer/choosescreen.visible = false
+	$CanvasLayer/ingamescreen.visible = true
+	$CanvasLayer/choosescreen.queue_free()
+
+
+func _on_button_mute_pressed() -> void:
+	if global.mute == false:
+		global.mute = true
+		$CanvasLayer/ingamescreen/ButtonMute/LabelOn.visible = true
+		$CanvasLayer/ingamescreen/ButtonMute/LabelOff.visible = false
+	else:
+		global.mute = false
+		$CanvasLayer/ingamescreen/ButtonMute/LabelOn.visible = false
+		$CanvasLayer/ingamescreen/ButtonMute/LabelOff.visible = true
+
+
+
+func _on_button_menu_pressed() -> void:
+	global.reset_values()
+	get_tree().reload_current_scene()
+
+func _process(_delta):
+	if global.game_over == true:
+		$CanvasLayer/ingamescreen.visible = false
+		$CanvasLayer/gameoverscreen.visible = true
+	$CanvasLayer/ingamescreen/LabelScore.text = str(global.score)
+	$CanvasLayer/gameoverscreen/LabelScore.text = "Score : " + str(global.score)
